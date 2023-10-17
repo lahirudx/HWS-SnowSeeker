6ac25dfa84edab451dca16ab166ee3982b5cf492
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-10-17 17:58:35 +0530

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Lahiru Amarasooriya on 2023-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Lahiru Amarasooriya on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
